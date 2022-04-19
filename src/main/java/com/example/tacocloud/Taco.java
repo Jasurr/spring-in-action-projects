8c1f8cff0934d6676576cc519e34d7c1345a7f73
jasur <njasur.1995@gmail.com>
2022-04-19 22:34:58 +0500

    String data JPA

--- src/main/java/com/example/tacocloud/Taco.java
+++ src/main/java/com/example/tacocloud/Taco.java
@@ -2,6 +2,7 @@ package com.example.tacocloud;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
@@ -9,20 +10,22 @@ import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    private Date createdAt = new Date();
 
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<IngredientRef> ingredients = new ArrayList<>();
+    @ManyToMany
+    private List<Ingredient> ingredients = new ArrayList<>();
 
-    public void addIngredient(Ingredient taco) {
-        this.ingredients.add(new IngredientRef(taco.getId()));
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
     }
 }
